983c26dde0b954b830d77c96673bf535c858721c
traianistrati-dev <traian.istrati@gmail.com>
2026-04-29 08:59:52 +0300

    [Sprint 1][Task 5] refactor: move endpoint logic from lib.rs to routes

--- auth-service/src/lib.rs
+++ auth-service/src/lib.rs
@@ -1,22 +1,19 @@
-use axum::response::IntoResponse;
 use axum::routing::post;
 use axum::Router;
 use std::error::Error;
 use tower_http::services::ServeDir;
 
+mod routes;
+use routes::*;
+
 // This struct encapsulates our application-related logic.
 pub struct Application {
     server: axum::serve::Serve<tokio::net::TcpListener, Router, Router>,
-    // address is exposed as a public field
-    // so we have access to it in tests.
     pub address: String,
 }
 
 impl Application {
     pub async fn build(address: &str) -> Result<Self, Box<dyn Error>> {
-        // Move the Router definition from `main.rs` to here.
-        // Also, remove the `hello` route.
-        // We don't need it at this point!
         let assets_dir = ServeDir::new("assets");
         let router = Router::new()
             .fallback_service(assets_dir)
@@ -30,31 +27,10 @@ impl Application {
         let address = listener.local_addr()?.to_string();
         let server = axum::serve(listener, router);
 
-        // Create a new Application instance and return it
         Ok(Self { address, server })
     }
     pub async fn run(self) -> Result<(), std::io::Error> {
         println!("listening on {}", &self.address);
         self.server.await
     }
-}
-
-async fn signup() -> impl IntoResponse {
-    axum::http::StatusCode::OK.into_response()
-}
-
-async fn login() -> impl IntoResponse {
-    axum::http::StatusCode::OK.into_response()
-}
-
-async fn logout() -> impl IntoResponse {
-    axum::http::StatusCode::OK.into_response()
-}
-
-async fn verify_2fa() -> impl IntoResponse {
-    axum::http::StatusCode::OK.into_response()
-}
-
-async fn verify_token() -> impl IntoResponse {
-    axum::http::StatusCode::OK.into_response()
 }
