e2476d668836c0e83727eca8f8785bfc2f0e61ce
traianistrati-dev <traian.istrati@gmail.com>
2026-05-11 12:42:06 +0300

    [Sprint 3][Test 3] feat: remove coockie at the end of logout

--- auth-service/src/routes/logout.rs
+++ auth-service/src/routes/logout.rs
@@ -1,5 +1,5 @@
 use axum::{http::StatusCode, response::IntoResponse};
-use axum_extra::extract::CookieJar;
+use axum_extra::extract::{cookie, CookieJar};
 
 use crate::{
     domain::AuthAPIError,
@@ -17,6 +17,8 @@ pub async fn logout(jar: CookieJar) -> (CookieJar, Result<impl IntoResponse, Aut
     if validate_token(&token).await.is_err() {
         return (jar, Err(AuthAPIError::InvalidToken));
     }
+
+    let jar = jar.remove(cookie::Cookie::from(JWT_COOKIE_NAME));
 
     (jar, Ok(StatusCode::OK))
 }
